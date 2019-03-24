37b32e254aeb49bb2caab87c5be39de514e7ae4f
jcasas5253 <skedabble@gmail.com>
2019-03-24 13:51:55 -0700

    MySql connection

---
+++ bamazoncustomer.js
@@ -0,0 +1,18 @@
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: 'root',
+	database: 'Bamazon'
+});
+
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('connected');
+});
+ 
